3abe758944e117c5551e6133f885fca0ad974fdf
Bangtian Liu <liubangtian@gmail.com>
2025-01-09 20:26:53 -0600

    fix mypy issue
    Signed-off-by: Bangtian Liu <liubangtian@gmail.com>

--- tuner/tuner/candidate_gen.py
+++ tuner/tuner/candidate_gen.py
@@ -349,7 +349,7 @@ def main() -> None:
             prefetch_shared_memory=args.prefetch_shared_memory_options,
             no_reduce_shared_memory_bank_conflicts=args.no_reduce_shared_memory_bank_conflicts_options,
         )
-        specs = generate_configs_and_td_specs(
+        specs: list[ir.Module] = generate_configs_and_td_specs(
             mlir_module,
             tuner_ctx,
             args.limit,
@@ -363,7 +363,7 @@ def main() -> None:
             spec_path = spec_dir / f"{candidate_num}_spec.mlir"
             spec_dir.mkdir(parents=True, exist_ok=True)
             with open(spec_path, "w") as f:
-                local_scope_spec_str = spec.operation.get_asm(use_local_scope=True)
+                local_scope_spec_str: str = spec.operation.get_asm(use_local_scope=True)
                 f.write(local_scope_spec_str)
 
 
