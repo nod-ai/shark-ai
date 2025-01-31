88e82fc86a0c465064a026cd8ed09814d32dd42a
aviator19941 <avinash.sharma@amd.com>
2025-01-31 15:06:13 -0600

    Fix fp8 8b benchmarking test
    Signed-off-by: aviator19941 <avinash.sharma@amd.com>

--- sharktank/tests/models/llama/benchmark_amdgpu_test.py
+++ sharktank/tests/models/llama/benchmark_amdgpu_test.py
@@ -66,10 +66,10 @@ class BenchmarkLlama3_1_8B(BaseBenchmarkTest):
     def setUp(self):
         super().setUp()
         # TODO: add numpy files to Azure and download from it
-        self.artifacts_dir = Path("/shark-dev/data/llama3.1/weights/8b")
-        self.artifacts_dir_2048 = Path("/shark-dev/8b")
-        self.irpa_path = self.artifacts_dir / "fp16/llama3.1_8b_fp16.irpa"
-        self.irpa_path_fp8 = self.artifacts_dir / "f8/llama3.1_8b_fp8.irpa"
+        self.artifacts_dir = Path("/shark-dev/8b")
+        self.weights_dir = self.artifacts_dir / "instruct/weights"
+        self.irpa_path = self.weights_dir / "llama3.1_8b_instruct_fp16.irpa"
+        self.irpa_path_fp8 = self.weights_dir / "fp8/native_fp8_e4m3fnuz_llama3_8b.irpa"
         self.tensor_parallelism_size = 1
         self.dir_path_8b = self.dir_path / "llama-8b"
         self.temp_dir_8b = Path(self.dir_path_8b)
@@ -80,15 +80,6 @@ class BenchmarkLlama3_1_8B(BaseBenchmarkTest):
             iree_hip_target="gfx942",
             iree_hal_target_device="hip",
             attention_kernel="torch",
-            tensor_parallelism_size=self.tensor_parallelism_size,
-            block_seq_stride=32,
-        )
-        self.llama8b_fp8_decomposed_artifacts = ExportArtifacts(
-            irpa_path=str(self.irpa_path_fp8),
-            batch_size=4,
-            iree_hip_target="gfx942",
-            iree_hal_target_device="hip",
-            attention_kernel="decomposed",
             tensor_parallelism_size=self.tensor_parallelism_size,
             block_seq_stride=32,
         )
@@ -104,16 +95,16 @@ class BenchmarkLlama3_1_8B(BaseBenchmarkTest):
             attention_dtype="float8_e4m3fnuz",
         )
         self.prefill_args_bs4_128_stride_32_f16 = (
-            self.artifacts_dir / "prefill_args_bs4_128_stride_32"
+            self.artifacts_dir / "prefill_args_bs4_128_stride_32_tp1"
         )
         self.decode_args_bs4_128_stride_32_f16 = (
-            self.artifacts_dir / "decode_args_bs4_128_stride_32"
+            self.artifacts_dir / "decode_args_bs4_128_stride_32_tp1"
         )
         self.prefill_args_bs4_2048_stride_32_f16 = (
-            self.artifacts_dir_2048 / "prefill_args_bs4_2048_stride_32"
+            self.artifacts_dir / "prefill_args_bs4_2048_stride_32"
         )
         self.decode_args_bs4_2048_stride_32_f16 = (
-            self.artifacts_dir_2048 / "decode_args_bs4_2048_stride_32"
+            self.artifacts_dir / "decode_args_bs4_2048_stride_32"
         )
         self.prefill_args_fp8 = self.artifacts_dir / "prefill_args_fp8"
         self.decode_args_fp8 = self.artifacts_dir / "decode_args_fp8"
@@ -283,7 +274,7 @@ class BenchmarkLlama3_1_8B(BaseBenchmarkTest):
 
     @pytest.mark.xfail(
         reason="Benchmark inputs not configured yet.",
-        strict=False,
+        strict=True,
         raises=IreeBenchmarkException,
     )
     def testBenchmark8B_fp8_Non_Decomposed(self):
