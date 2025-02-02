8300bc8f89af27c6c782d36ecb72fa419d4e7875
dan <danimal197@gmail.com>
2025-02-01 16:13:49 -0800

    restore custom matmul kernel

--- sharktank/sharktank/ops/qlinear_impls.py
+++ sharktank/sharktank/ops/qlinear_impls.py
@@ -50,10 +50,10 @@ def qlinear_tensor_scaled(
 
     # Handle only integer and fp8 quantizations.
     if x_layout.qs.dtype.is_floating_point or weight_layout.qs.dtype.is_floating_point:
-        if x_layout.qs.dtype == torch.float8_e4m3fnuz:
-            # assume quark
-            return matmul(x_layout.qs, weight_layout.qs, transpose_rhs=True)
-        else:
+        if (
+            x_layout.qs.dtype != torch.float8_e4m3fnuz
+            or weight_layout.qs.dtype != torch.float8_e4m3fnuz
+        ):
             return NotImplemented
 
     # Bias.
