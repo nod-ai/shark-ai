f664fcbf118b7adff78103a69c9a426dedafa2f2
Ian <ian.nordeng@amd.com>
2025-02-20 19:49:45 -0600

    Some dtype fixes for vae tests

--- sharktank/tests/models/vae/vae_test.py
+++ sharktank/tests/models/vae/vae_test.py
@@ -277,7 +277,7 @@ class VaeFluxDecoderTest(TempDirTestBase):
         model_f32 = VaeDecoderModel.from_dataset(ds_f32).to(device="cpu")
 
         # TODO: Decomposing attention due to https://github.com/iree-org/iree/issues/19286, remove once issue is resolved
-        module = export_vae(model, inputs, True)
+        module = export_vae(model, inputs.to(dtype=dtype), True)
         module_f32 = export_vae(model_f32, inputs, True)
 
         module.save_mlir("{self._temp_dir}/flux_vae_bf16.mlir")
