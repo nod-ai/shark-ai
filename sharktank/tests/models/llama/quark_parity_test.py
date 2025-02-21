475309f2c8f31b09853571ea2532f36c66cf6710
Elias Joseph <eljoseph@amd.com>
2025-02-21 01:21:27 -0600

    tried to fix path in sharktank
    Signed-off-by: Elias Joseph <eljoseph@amd.com>

--- sharktank/tests/models/llama/quark_parity_test.py
+++ sharktank/tests/models/llama/quark_parity_test.py
@@ -25,7 +25,7 @@ class QuarkParityTest(unittest.TestCase):
         sharktank_dir = str(
             Path(os.path.dirname(os.path.abspath(__file__))).parent.parent.parent.parent
         )
-        our_path = "ours_prefill.safetensors"
+        our_path = self.path_prefix / "ours_prefill.safetensors"
         if os.path.exists(our_path):
             os.remove(our_path)
 
@@ -58,12 +58,13 @@ class QuarkParityTest(unittest.TestCase):
             "--fake-quant",
             "--attention-kernel=torch",
             "--activation-dtype=bfloat16",
-            f"--save_intermediates_path=ours",
+            f"--save_intermediates_path={self.path_prefix}/ours",
             "--use-hf",
             "--attention-dtype=bfloat16",
             "--skip-decode",
             "--block-seq-stride=16",
         ]
+        f = open("/shark-cache/quark_test/test.txt", "w+")
         command = subprocess.list2cmdline(command)
         proc = subprocess.run(
             command, shell=True, capture_output=True, cwd=sharktank_dir
