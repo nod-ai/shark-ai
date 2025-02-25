800c6b3210f83517f2292ade1dd204e5cd3706d4
archana-ramalingam <archana.ramalingam@amd.com>
2025-02-25 13:53:01 -0800

    Fix pre-commit error

--- sharktank/sharktank/evaluate/perplexity_iree.py
+++ sharktank/sharktank/evaluate/perplexity_iree.py
@@ -156,7 +156,7 @@ class Perplexity:
             use_attention_mask=self.use_attention_mask,
             activation_dtype=str(self.activation_dtype).split(".")[-1],
             attention_dtype=str(self.attention_dtype).split(".")[-1],
-            kv_cache_dtype = str(self.kv_cache_dtype).split(".")[-1],
+            kv_cache_dtype=str(self.kv_cache_dtype).split(".")[-1],
             use_hf=self.use_hf,
         )
 
