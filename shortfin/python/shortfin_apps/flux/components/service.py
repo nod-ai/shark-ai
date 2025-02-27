f2feef2153cd83ef22fadd18f0f5c2a59a70a3dd
Kyle Herndon <kyle.herndon@amd.com>
2025-02-27 22:11:19 +0000

    Refactor the SystemManager across shortfin apps

--- shortfin/python/shortfin_apps/flux/components/service.py
+++ shortfin/python/shortfin_apps/flux/components/service.py
@@ -21,7 +21,7 @@ import shortfin.array as sfnp
 from ...utils import GenerateService, BatcherProcess
 
 from .config_struct import ModelParams
-from .manager import SystemManager
+from .manager import FluxSystemManager
 from .messages import FluxInferenceExecRequest, InferencePhase
 from .tokenizer import Tokenizer
 from .metrics import measure
@@ -70,7 +70,7 @@ class FluxGenerateService(GenerateService):
         self,
         *,
         name: str,
-        sysman: SystemManager,
+        sysman: FluxSystemManager,
         clip_tokenizers: list[Tokenizer],
         t5xxl_tokenizers: list[Tokenizer],
         model_params: ModelParams,
