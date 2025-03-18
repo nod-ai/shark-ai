095f0eeb0923ff8a58c64de7cc6fac9a7c62a650
Rob Suderman <rob.suderman@gmail.com>
2025-03-18 15:56:26 -0700

    tweak again

--- shortfin/python/shortfin_apps/llm/components/batcher.py
+++ shortfin/python/shortfin_apps/llm/components/batcher.py
@@ -468,7 +468,8 @@ class DecodeExecutorProcess(LlmExecutorProcess):
             for i in range(bs):
                 if i < req_count:
                     batch_ids = self.exec_requests[i].cache_page_indices(block_count)
-                    batch_ids += [0] * (block_count - len(batch_ids))
+                    block_ids += batch_ids
+                    block_ids += [0] * (block_count - len(batch_ids))
             m.items = block_ids
 
         # Transfer to device memory:
