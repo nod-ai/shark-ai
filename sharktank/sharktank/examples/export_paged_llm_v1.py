63ea8be095f10958b94fbdeb5ae93f02c83b8e8c
aviator19941 <avinash.sharma@amd.com>
2025-03-18 16:44:05 -0700

    Add attn_dtype to model config for shortfin, start sfnp float8 support
    Signed-off-by: aviator19941 <avinash.sharma@amd.com>

--- sharktank/sharktank/examples/export_paged_llm_v1.py
+++ sharktank/sharktank/examples/export_paged_llm_v1.py
@@ -123,6 +123,9 @@ def main():
             "prefill_batch_sizes": prefill_bs,
             "decode_batch_sizes": decode_bs,
             "transformer_block_count": hp.block_count,
+            "attn_dtype": str(llama_config.kv_cache_dtype).split(".")[
+                -1
+            ],  # convert torch.bfloat16 dtype into "bfloat16"
             "paged_kv_cache": {
                 "attention_head_count_kv": hp.attention_head_count_kv,
                 "block_seq_stride": llama_config.block_seq_stride,
