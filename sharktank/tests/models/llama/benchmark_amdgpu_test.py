b2810d2b765b3b3a66e5d43b1b939c108d884d40
aviator19941 <avinash.sharma@amd.com>
2025-04-05 22:54:01 +0000

    Use irpa_path_fp8_attnf8 to see if file is missing
    Signed-off-by: aviator19941 <avinash.sharma@amd.com>

--- sharktank/tests/models/llama/benchmark_amdgpu_test.py
+++ sharktank/tests/models/llama/benchmark_amdgpu_test.py
@@ -127,7 +127,7 @@ class BenchmarkLlama3_1_8B(BaseBenchmarkTest):
             block_seq_stride=32,
         )
         self.llama8b_fp8_torch_sdpa_artifacts = ExportArtifacts(
-            irpa_path=str(self.irpa_path_fp8),
+            irpa_path=str(self.irpa_path_fp8_attnf8),
             batch_size=4,
             iree_hip_target="gfx942",
             iree_hal_target_device="hip",
@@ -138,6 +138,7 @@ class BenchmarkLlama3_1_8B(BaseBenchmarkTest):
             activation_dtype="bfloat16",
             attention_dtype="bfloat16",
             kv_cache_dtype="float8_e4m3fnuz",
+            use_attention_mask=True,
         )
         self.llama8b_fp8_attnf8_sdpa_artifacts = ExportArtifacts(
             irpa_path=str(self.irpa_path_fp8_attnf8),
