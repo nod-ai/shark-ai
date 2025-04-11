7e746b8ba5ebe5676faa73a3aaf2d8ea4d59aa4f
Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>
2025-04-11 08:47:33 -0700

    export_paged_llm_v1
    Signed-off-by: Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>

--- sharktank/sharktank/examples/export_paged_llm_v1.py
+++ sharktank/sharktank/examples/export_paged_llm_v1.py
@@ -66,6 +66,7 @@ def main():
     llama_config = LlamaModelConfig(
         hp,
         tensor_parallelism_size=tensor_parallelism_size,
+        pipeline_parallelism_size=args.pipeline_parallelism_size,
         use_hf=args.use_hf,
         static_tables=False,  # Rely on the compiler for hoisting tables.
         attention_kernel=args.attention_kernel,
