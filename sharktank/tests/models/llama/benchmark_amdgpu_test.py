28a5222c3cea54a24251f4c6d42342b8615ea063
archana-ramalingam <archana.ramalingam@amd.com>
2025-04-12 00:13:20 +0000

    xfail fp8 (fp8 attn) test

--- sharktank/tests/models/llama/benchmark_amdgpu_test.py
+++ sharktank/tests/models/llama/benchmark_amdgpu_test.py
@@ -368,6 +368,7 @@ class BenchmarkLlama3_1_8B(BaseBenchmarkTest):
         )
 
     @skipif_run_quick_llama_test
+    @pytest.mark.xfail(run=False, reason="Compile Error", raises=IreeCompileException)
     def testBenchmark8B_fp8_attnf8_TP1_Non_Decomposed_Input_Len_2048(self):
         output_file_name = self.dir_path_8b / "fp8_attnf8_2048_tp1"
         output_mlir = self.llama8b_fp8_attnf8_sdpa_artifacts.create_file(
