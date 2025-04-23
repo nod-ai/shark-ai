d5cadce6a9fc17858b882f3532b15bb1bc568be2
Ian Wood <ianwood2024@u.northwestern.edu>
2025-04-23 12:37:27 -0700

    Handle input extf in kernel
    Signed-off-by: Ian Wood <ianwood2024@u.northwestern.edu>

--- sharktank/sharktank/kernels/rotary.py
+++ sharktank/sharktank/kernels/rotary.py
@@ -4,24 +4,45 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+import torch
+from iree.turbine.support.conversions import (
+    IREE_TYPE_ASM_TO_TORCH_DTYPE,
+    TORCH_DTYPE_TO_IREE_TYPE_ASM,
+)
+from typing import Optional, cast
+from iree.compiler.ir import Type
 from sharktank.kernels.base import *
+from iree.turbine.runtime.op_reg import AttrArg
 
 __all__ = [
     "apply_rotary_embedding",
 ]
 
 
-@CustomOp.register(library=LIBRARY)
-class apply_rotary_embedding(CustomOp):
+def apply_rotary_embedding(
+    input: torch.Tensor, table: torch.Tensor, /, *, dtype: Optional[torch.dtype] = None
+):
+    if dtype is None:
+        dtype = input.dtype
+    assert dtype == table.dtype
+    return _apply_rotary_embedding(
+        input=input, table=table, dtype=TORCH_DTYPE_TO_IREE_TYPE_ASM[dtype]
+    )
 
-    signature = "apply_rotary_embedding(Tensor input, Tensor table) -> (Tensor)"
+
+@CustomOp.register(library=LIBRARY)
+class _apply_rotary_embedding(CustomOp):
+
+    signature = (
+        "apply_rotary_embedding(Tensor input, Tensor table, str dtype) -> (Tensor)"
+    )
 
     def select(self, ksel: KernelSelection):
         inputs_desc = ksel.arg_tensor(0)
         table_desc = ksel.arg_tensor(1)
-        out_desc = ksel.return_new_tensor(
-            inputs_desc.t.shape, dtype=inputs_desc.t.dtype
-        )
+        dtype_desc = ksel.attr_str(2)
+        dtype = IREE_TYPE_ASM_TO_TORCH_DTYPE[dtype_desc.v]
+        out_desc = ksel.return_new_tensor(inputs_desc.t.shape, dtype=dtype)
         specialize_all_known_dims(inputs_desc)
         specialize_all_known_dims(table_desc)
         specialize_all_known_dims(out_desc)
@@ -30,14 +51,15 @@ class apply_rotary_embedding(CustomOp):
 
         input = kb.arg_value(0)
         table = kb.arg_value(1)
+        dtype_str = cast(AttrArg, ksel.arg_descs[2]).v
+        dtype = Type.parse(dtype_str)
 
         input_tensor_type = RankedTensorType(input.type)
         table_tensor_type = RankedTensorType(table.type)
+        output_tensor_type = RankedTensorType.get(input_tensor_type.shape, dtype)
 
         input_asm_type, input_ident, input_dtype = unpack_tensor_type(input.type)
         table_asm_type, table_ident, table_dtype = unpack_tensor_type(table.type)
-
-        assert input_dtype == table_dtype
 
         # Generate specialization signature and types.
         bs = input.type.shape[0]
@@ -47,9 +69,7 @@ class apply_rotary_embedding(CustomOp):
         dims = input.type.shape[3]
 
         template_file = "rotary_embedding.mlir"
-        target_function_name = (
-            f"sharktank_rotary_embedding_{bs}_{sl}_{heads}_{dims}_{input_dtype}"
-        )
+        target_function_name = f"sharktank_rotary_embedding_{bs}_{sl}_{heads}_{dims}_{input_dtype}_{table_dtype}_{dtype}"
 
         # Template params.
         input_tensor_type = input_asm_type
@@ -59,12 +79,16 @@ class apply_rotary_embedding(CustomOp):
             kb,
             template_file,
             target_function_name,
+            func_name=target_function_name,
             input_tensor_type=input_tensor_type,
             table_tensor_type=table_tensor_type,
+            output_tensor_type=str(output_tensor_type),
             bs=bs,
             sl=sl,
             heads=heads,
             dims=dims,
-            dtype=str(input_dtype),
+            input_dtype=str(input_dtype),
+            table_dtype=str(table_dtype),
+            dtype=str(dtype),
         )
         kb.yield_results(*call_function(target_function, *kb.arg_bindings))
---
+++ sharktank/tests/kernels/rotary_test.py
@@ -0,0 +1,52 @@
+# Copyright 2024 Advanced Micro Devices, Inc.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+import torch
+import unittest
+from parameterized import parameterized
+
+from sharktank import kernels
+from sharktank import ops
+
+
+class rotary_test(unittest.TestCase):
+    def setUp(self):
+        torch.manual_seed(42)
+
+    @parameterized.expand([(1, 1), (4, 1), (1, 64), (4, 64)])
+    def test_rotary(self, bs, seq_len):
+        dtype = torch.float32
+        a = torch.rand([bs, 128, seq_len, 64], dtype=dtype)
+        rot = torch.rand([bs, 128, 32], dtype=dtype)
+        res_b = ops.view_as_real(torch.complex(rot, rot))
+        ref_b = torch.complex(torch.cos(rot), torch.sin(rot))
+
+        result = kernels.apply_rotary_embedding(a, res_b)
+        ref = ops.view_as_real(ops.view_as_complex(a) * ref_b.unsqueeze(2))
+        torch.testing.assert_close(result, ref)
+
+    @parameterized.expand([(1, 1), (4, 1), (1, 64), (4, 64)])
+    def test_upcast(self, bs, seq_len):
+        narrow_dtype = torch.float16
+        wide_dtype = torch.float32
+        a = torch.rand([bs, 128, seq_len, 64], dtype=narrow_dtype)
+        rot = torch.rand([bs, 128, 32], dtype=wide_dtype)
+        res_b = ops.view_as_real(torch.complex(rot, rot))
+        ref_b = torch.complex(torch.cos(rot), torch.sin(rot))
+
+        result = kernels.apply_rotary_embedding(a, res_b, dtype=wide_dtype)
+        ref = ops.view_as_real(
+            ops.view_as_complex(ops.to(a, dtype=wide_dtype)) * ref_b.unsqueeze(2)
+        )
+        torch.testing.assert_close(result, ref)
+
+
+if __name__ == "__main__":
+    unittest.main()
