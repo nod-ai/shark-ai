f5f6af0115260c557e9f103c083aa24b4a718684
Kunwar Grover <groverkss@gmail.com>
2025-05-28 17:15:02 +0100

    fix typo

--- sharktank/sharktank/layers/rotary_embedding.py
+++ sharktank/sharktank/layers/rotary_embedding.py
@@ -41,7 +41,7 @@ class RotaryEmbeddingLayer(BaseLayer):
         self.rope_dimension_count = rope_dimension_count
         self.max_seqlen = max_seqlen
         self.use_hf = use_hf
-        self.rope_scaling_type = (rope_scaling_type,)
+        self.rope_scaling_type = rope_scaling_type
         self.use_table = use_table
         self.dtype = dtype
         self.rope_freq_base = rope_freq_base if rope_freq_base is not None else 10000.0
