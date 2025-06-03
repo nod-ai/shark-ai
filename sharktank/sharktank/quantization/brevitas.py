fb4bb50ced189971d56d9c3bd6aef233c1f5c777
Kyle Herndon <kyle.herndon@amd.com>
2025-06-03 23:01:41 +0000

    Update copyright

--- sharktank/sharktank/quantization/brevitas.py
+++ sharktank/sharktank/quantization/brevitas.py
@@ -1,3 +1,9 @@
+# Copyright 2025 Advanced Micro Devices, Inc.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 from brevitas.graph.quantize import quantize as brevitas_quantize
 from brevitas.quant.scaled_int import (
     Int8WeightPerTensorFloat,
