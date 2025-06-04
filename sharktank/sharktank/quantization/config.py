80e3b24f9b82cbb6fd7b849e0c05057b7916135e
Kyle Herndon <kyle.herndon@amd.com>
2025-06-04 21:14:04 +0000

    Pre-commit

--- sharktank/sharktank/quantization/config.py
+++ sharktank/sharktank/quantization/config.py
@@ -1,4 +1,3 @@
-
 from typing import Optional, Union
 
 import torch
@@ -21,11 +20,12 @@ torch_to_brevitas = {
     torch.int8: Int8WeightPerTensorFloat,
 }
 
-class QuantizationConfig():
-    def __init__(self,
-                 dtype: Optional[Union[torch.dtype, str]] = torch.float8_e4m3fnuz ):
+
+class QuantizationConfig:
+    def __init__(
+        self, dtype: Optional[Union[torch.dtype, str]] = torch.float8_e4m3fnuz
+    ):
         if isinstance(dtype, str):
             dtype = string_to_torch[dtype]
         self.dtype = dtype
         self.quantization_scheme = torch_to_brevitas[dtype]
-        
