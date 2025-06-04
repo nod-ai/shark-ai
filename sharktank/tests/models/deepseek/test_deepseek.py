59c76944dfaf4b2c9bc9a49bc80d59ffa92d4cf4
Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>
2025-06-04 16:03:22 +0000

    Compare all logits
    Signed-off-by: Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>

--- sharktank/tests/models/deepseek/test_deepseek.py
+++ sharktank/tests/models/deepseek/test_deepseek.py
@@ -145,21 +145,21 @@ class DeepseekTest(TempDirTestBase):
             iree_logits = iree_result[0]
             return iree_logits
 
-        iree_logits_i = with_iree_device_context(run_iree_module, iree_devices)
+        iree_logits = with_iree_device_context(run_iree_module, iree_devices)
         iree_cache_state_after = deepcopy(iree_cache_state)
 
         # Compare logits
-        padding_mask = (
-            (token_ids != 0).int().detach().clone().to(token_ids.device).bool()
-        )
-        all_ref_logits, all_iree_logits = [], []
         for i in range(len(ids)):
-            all_ref_logits.append(reference_logits[i, padding_mask[i]])
-            all_iree_logits.append(iree_logits_i[i, padding_mask[i]])
+            same = torch.isclose(
+                reference_logits[i], iree_logits[i], rtol=1.3e-6, atol=1e-5
+            )
+            if not same.all():
+                raise AssertionError(
+                    f"Logits mismatch for batch {i}: "
+                    f"Num mismatch: {(~same).sum()}. {100*same.sum() / same.numel():.1f}% match."
+                )
 
-        for ref_logits_i, iree_logits_i in zip(all_ref_logits, all_iree_logits):
-            assert ref_logits_i.shape == iree_logits_i.shape
-            torch.testing.assert_close(ref_logits_i, iree_logits_i)
+            torch.testing.assert_close(reference_logits[i], iree_logits[i])
 
         # Compare cache state
         assert len(iree_cache_state_after) == len(reference_cache_state_after)
