c3d6127aea631f175063879c1840b2e13662ba3c
Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>
2025-06-17 15:25:18 +0000

    Add 70B parser flags
    Signed-off-by: Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>

--- sharktank/conftest.py
+++ sharktank/conftest.py
@@ -155,6 +155,25 @@ def pytest_addoption(parser):
         help="Llama3.1 8b f8 model path",
     )
     parser.addoption(
+        "--llama3-70b-tokenizer-path",
+        type=Path,
+        action="store",
+        help="Llama3.1 70b tokenizer path",
+    )
+    parser.addoption(
+        "--llama3-70b-f16-model-path",
+        type=Path,
+        action="store",
+        help="Llama3.1 70b model path",
+    )
+    parser.addoption(
+        "--llama3-70b-f8-model-path",
+        type=Path,
+        action="store",
+        default=None,
+        help="Llama3.1 70b f8 model path",
+    )
+    parser.addoption(
         "--llama3-405b-tokenizer-path",
         type=Path,
         action="store",
@@ -330,6 +349,15 @@ def get_model_artifacts(request: FixtureRequest):
     )
     model_path["llama3_8b_f8_model_path"] = set_fixture_from_cli_option(
         request, "--llama3-8b-f8-model-path", "llama3_8b_f8_model"
+    )
+    model_path["llama3_70b_tokenizer_path"] = set_fixture_from_cli_option(
+        request, "--llama3-70b-tokenizer-path", "llama3_70b_tokenizer"
+    )
+    model_path["llama3_70b_f16_model_path"] = set_fixture_from_cli_option(
+        request, "--llama3-70b-f16-model-path", "llama3_70b_f16_model"
+    )
+    model_path["llama3_70b_f8_model_path"] = set_fixture_from_cli_option(
+        request, "--llama3-70b-f8-model-path", "llama3_70b_f8_model"
     )
     model_path["llama3_405b_tokenizer_path"] = set_fixture_from_cli_option(
         request, "--llama3-405b-tokenizer-path", "llama3_405b_tokenizer"
