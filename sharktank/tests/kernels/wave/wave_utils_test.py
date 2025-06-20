aea00596b6c64ea281ae50fa51ea86f802785b23
aviator19941 <avinash.sharma@amd.com>
2025-06-20 08:45:55 -0700

    Add copyright to wave_utils_test
    Signed-off-by: aviator19941 <avinash.sharma@amd.com>

--- sharktank/tests/kernels/wave/wave_utils_test.py
+++ sharktank/tests/kernels/wave/wave_utils_test.py
@@ -1,3 +1,9 @@
+# Copyright 2025 Advanced Micro Devices, Inc.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 from iree.compiler.ir import Context, Module
 from iree.compiler.passmanager import PassManager
 from sharktank.kernels.wave.utils import get_wave_module_body_asm
