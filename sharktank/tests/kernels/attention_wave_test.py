85f4fd1d46298912eef0d438785090eece47ef42
aviator19941 <avinash.sharma@amd.com>
2025-07-01 15:31:36 +0000

    Fix dynamic case of Wave kernel integration into sharktank
    Signed-off-by: aviator19941 <avinash.sharma@amd.com>

--- sharktank/tests/kernels/attention_wave_test.py
+++ sharktank/tests/kernels/attention_wave_test.py
@@ -44,12 +44,12 @@ class wave_attention(unittest.TestCase):
             mlir_asm,
         )
         self.assertIn(
-            ("func.func private @wave_flash_attention_4_32_128_128_f16_f32"),
+            ("func.func private @wave_flash_attention_4_32_dyn_128_f16_f32"),
             mlir_asm,
         )
         self.assertIn(
             (
-                "util.func private @wave_bhsd_flash_attention_B_4_H_32_M_128_K1_128_f16_B_4_H_32_K2_128_K1_128_f16_B_4_H_32_K2_128_N_128_f16_B_4_H_32_M_128_N_128_f32_B_4_H_32_M_128_N_128_f32"
+                "util.func private @wave_bhsd_flash_attention_B_4_H_32_M_K1_128_f16_B_4_H_32_K2_K1_128_f16_B_4_H_32_K2_N_128_f16_B_4_H_32_M_N_128_f32_B_4_H_32_M_N_128_f32"
             ),
             mlir_asm,
         )
