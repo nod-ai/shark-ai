c1713dc768acb29634800dcd6e40fccb4a6896ac
Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>
2025-07-08 19:46:24 +0000

    Add quantizertensor support for barrier, transfer, and replicate
    Signed-off-by: Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>

--- sharktank/sharktank/ops/quantized_impls.py
+++ sharktank/sharktank/ops/quantized_impls.py
@@ -8,45 +8,44 @@
 from typing import Callable
 
 from torch import Tensor
-from sharktank.types.layouts import BlockScaledFp4Layout
 from ._registry import *
-from sharktank.types.tensors import (
-    PlanarQuantizedTensor,
-    ReplicatedTensor,
-    QuantizedTensor,
-)
+from sharktank.types.tensors import ReplicatedTensor, QuantizedTensor
+from sharktank.types.quantizers import QuantizerTensor
+
 from .signatures import *
 
 import iree.turbine.ops.iree
 
 
-@replicate.override(QuantizedTensor)
+@replicate.override(AnyOfType(QuantizedTensor, QuantizerTensor))
 def replicate_quantized(
-    input: QuantizedTensor, *, count: int, devices: tuple[int, ...]
+    tensor: QuantizedTensor | QuantizerTensor, *, count: int, devices: tuple[int, ...]
 ) -> ReplicatedTensor:
     assert count == len(devices)
-    return ReplicatedTensor(ts=input, shard_count=count, devices=devices)
+    return ReplicatedTensor(ts=tensor, shard_count=count, devices=devices)
 
 
-@transfer_to_logical_device.override(QuantizedTensor)
+@transfer_to_logical_device.override(AnyOfType(QuantizedTensor, QuantizerTensor))
 def transfer_to_logical_device_planar_quantized_tensor(
-    tensor: QuantizedTensor, ordinal: int
+    tensor: QuantizedTensor | QuantizerTensor, ordinal: int
 ):
     return transfer_or_barrier(
         iree.turbine.ops.iree.transfer_to_logical_device, tensor, ordinal
     )
 
 
-@barrier_on_logical_device.override(QuantizedTensor)
+@barrier_on_logical_device.override(AnyOfType(QuantizedTensor, QuantizerTensor))
 def barrier_on_logical_device__planar_quantized_tensor(
-    tensor: QuantizedTensor, ordinal: int
+    tensor: QuantizedTensor | QuantizerTensor, ordinal: int
 ):
     return transfer_or_barrier(
         iree.turbine.ops.iree.barrier_on_logical_device, tensor, ordinal
     )
 
 
-def transfer_or_barrier(operation: Callable, tensor: QuantizedTensor, ordinal: int):
+def transfer_or_barrier(
+    operation: Callable, tensor: QuantizedTensor | QuantizerTensor, ordinal: int
+):
     def operation_transform(globals: dict[str, Tensor]) -> dict[str, Tensor]:
         return {k: operation(f"{ordinal}", v) for k, v in globals.items()}
 
