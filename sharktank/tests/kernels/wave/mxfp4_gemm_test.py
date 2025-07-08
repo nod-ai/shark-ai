868261e8e80625263b3fedb209d12bcda0380bc2
aviator19941 <avinash.sharma@amd.com>
2025-07-08 15:09:46 +0000

    Add wave_fp4_bmm test
    Signed-off-by: aviator19941 <avinash.sharma@amd.com>

---
+++ sharktank/tests/kernels/wave/mxfp4_gemm_test.py
@@ -0,0 +1,60 @@
+# Copyright 2025 Advanced Micro Devices, Inc.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+import unittest
+
+import torch
+from iree.compiler.passmanager import PassManager
+from iree.compiler.ir import Context, Module
+import iree.turbine.aot as aot
+from sharktank.kernels.wave.gemm import wave_mxfp4_gemm
+from parameterized import parameterized
+
+
+class wave_fp4_gemm(unittest.TestCase):
+    def test_wave_fp4_gemm(self):
+        class WaveMxfp4Module(torch.nn.Module):
+            def forward(self, x, x_scales, w_t, w_scales, output):
+                return wave_mxfp4_gemm(x, x_scales, w_t, w_scales, output)
+
+        e = aot.export(
+            WaveMxfp4Module(),
+            args=(
+                torch.empty((4, 1024, 512), dtype=torch.uint8),
+                torch.empty((4, 1024, 32), dtype=torch.uint8),
+                torch.empty((1024, 512), dtype=torch.uint8),
+                torch.empty((1024, 32), dtype=torch.uint8),
+                torch.empty((4, 1024, 1024), dtype=torch.float32),
+            ),
+        )
+        e.verify()
+        mlir_asm = str(e.mlir_module)
+        self.assertIn(
+            ("func.func @main"),
+            mlir_asm,
+        )
+        self.assertIn(
+            ("stream.executable private @batched_gemm"),
+            mlir_asm,
+        )
+        self.assertIn(
+            ("func.func private @wave_mxfp4_bmm_B_dyn_M_dyn_1024_ui8_f32"),
+            mlir_asm,
+        )
+        self.assertIn(
+            (
+                "util.func private @wave_util_mxfp4_bmm_B_M_K/2_512_ui8_B_M_K/32_32_ui8_N_K/2_512_ui8_N_K/32_32_ui8_B_M_N_1024_f32"
+            ),
+            mlir_asm,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
