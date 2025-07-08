b3ba525e14464eb9b52bdbc9d086a5ca45238ea2
Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>
2025-07-08 16:20:23 +0000

    More _clone_with_globals
    Signed-off-by: Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>

--- sharktank/sharktank/types/tensors.py
+++ sharktank/sharktank/types/tensors.py
@@ -1057,9 +1057,14 @@ class ShardedTensorBase(ShardedTensor):
     def _clone_with_globals(
         self, new_globals: dict[str, torch.Tensor]
     ) -> "InferenceTensor":
+        # NOTE: Assuming that the type of each shard does not change
+        all_new_keys = list(self.globals.keys())
         ts = []
-        for k in self.globals.keys():
-            ts.append(new_globals[k])
+        for i, shard in enumerate(self._shards):
+            shard_i_key = f".shard.{i}"
+            matching_keys = [k for k in all_new_keys if shard_i_key in k]
+            new_sub_globals = {k: new_globals.pop(k) for k in matching_keys}
+            ts.append(shard[i]._clone_with_globals(new_sub_globals))
         return self.__class__(
             name=self.name,
             shape=self.shape,
@@ -1425,7 +1430,7 @@ class ReplicatedTensor(ShardedTensor):
 
     def _clone_with_globals(
         self, new_globals: dict[str, torch.Tensor]
-    ) -> "InferenceTensor":
+    ) -> "ReplicatedTensor":
         # NOTE: Assuming that the type of each shard does not change
         all_new_keys = list(self.globals.keys())
         ts = []
