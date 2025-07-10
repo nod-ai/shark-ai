94afb0b8f4084dce545102434976aaf45dd1099b
Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>
2025-07-10 19:11:19 +0000

    Cleanup transpose for quantized tensor
    Signed-off-by: Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>

--- sharktank/sharktank/ops/default_impls.py
+++ sharktank/sharktank/ops/default_impls.py
@@ -748,16 +748,11 @@ def transpose_default(
 def transpose_PlanarQuantizedTensor(
     tensor: PlanarQuantizedTensor, dim0: int, dim1: int
 ):
-    # TODO: Can we use layour.shape instead?
-    new_shape = list(tensor.shape)
-    new_shape[dim0], new_shape[dim1] = new_shape[dim1], new_shape[dim0]
-
     new_layout = tensor.unpack().transpose(dim0, dim1)
     if not isinstance(new_layout, QuantizedLayout):
         return NotImplemented
-    new_shape = new_layout.sh
 
-    return PlanarQuantizedTensor(shape=new_shape, layout=new_layout)
+    return PlanarQuantizedTensor(shape=new_layout.shape, layout=new_layout)
 
 
 # Sharded default impls (do nothing).
