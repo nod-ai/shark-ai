f0ec18018ab6a995edfd4d52322824566e064afd
Sambhav Jain <sambhav.jain@amd.com>
2025-07-10 21:25:55 +0000

    conv_node test

---
+++ sharkfuser/tests/test_conv_node.cpp
@@ -0,0 +1,55 @@
+// Copyright 2025 Advanced Micro Devices, Inc.
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <fusili.h>
+
+#include <catch2/catch_test_macros.hpp>
+#include <memory>
+#include <vector>
+
+using namespace fusili;
+
+TEST_CASE("ConvFPropNode pre_validate_node detects missing attributes",
+          "[conv_node]") {
+  Context ctx;
+  ConvFPropAttr attr;
+
+  // Leave attributes empty to trigger errors
+  ConvFPropNode node(std::move(attr), ctx);
+  REQUIRE(node.pre_validate_node() == error_code_t::ATTRIBUTE_NOT_SET);
+}
+
+TEST_CASE("ConvFPropNode pre_validate_node passes with all attributes set",
+          "[conv_node]") {
+  Context ctx;
+  ConvFPropAttr attr;
+
+  attr.set_pre_padding({0, 0})
+      .set_post_padding({0, 0})
+      .set_stride({1, 1})
+      .set_dilation({1, 1});
+
+  ConvFPropNode node(std::move(attr), ctx);
+  REQUIRE(node.pre_validate_node().is_good());
+}
+
+TEST_CASE("ConvFPropNode infer_properties_node returns NOT_IMPLEMENTED",
+          "[conv_node]") {
+  Context ctx;
+  ConvFPropAttr attr;
+
+  attr.set_pre_padding({0, 0})
+      .set_post_padding({0, 0})
+      .set_stride({1, 1})
+      .set_dilation({1, 1});
+
+  attr.set_X(std::make_shared<TensorAttr>(1.0f));
+  attr.set_W(std::make_shared<TensorAttr>(2.0f));
+  attr.set_Y(std::make_shared<TensorAttr>());
+
+  ConvFPropNode node(std::move(attr), ctx);
+  REQUIRE(node.infer_properties_node() == error_code_t::NOT_IMPLEMENTED);
+}
