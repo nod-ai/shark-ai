5660cf70784c86849e78a6a94530ad9f93323fa8
Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>
2025-07-12 00:54:30 +0000

    --use-hf
    Signed-off-by: Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>

--- sharktank/tests/models/llama/test_llama.py
+++ sharktank/tests/models/llama/test_llama.py
@@ -75,16 +75,15 @@ class LlamaIreeVsEagerTest(TempDirTestBase):
     def testUnshardedToyIreeVsEager(
         self, tensor_parallelism_size: int, pipeline_parallelism_size: int, use_hf: bool
     ):
-        if use_hf:
-            pytest.xfail(reason="Expected failure needs to be fixed.")
-
         theta, config = generate(12345)
         config.tensor_parallelism_size = tensor_parallelism_size
         config.pipeline_parallelism_size = pipeline_parallelism_size
         config.use_hf = use_hf
 
+        # from pathlib import Path
         tester = IreeVsEagerLLMTester(
             work_dir=self._temp_dir,
+            # work_dir=Path("/home/alvasile/repos/shark-ai/dump"),
             theta=theta,
             config=config,
             torch_device=self.device,
