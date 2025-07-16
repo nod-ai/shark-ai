25ad69a28ff408de1678ff4f0fd831c1efee3fe0
Ean Garvey <ean.garvey@amd.com>
2025-07-16 02:45:46 +0000

    Update amdgpu -> hip

--- shortfin/python/shortfin_apps/sd/components/builders.py
+++ shortfin/python/shortfin_apps/sd/components/builders.py
@@ -63,12 +63,12 @@ def get_vmfb_filenames(
     model_params: ModelParams,
     model=None,
     target: str = "gfx942",
-    driver: str = "amdgpu",
+    driver: str = "hip",
 ) -> list:
     vmfb_filenames = []
     file_stems = get_file_stems(model_params)
     for stem in file_stems:
-        vmfb_filenames.extend([stem + "_" + driver + ".vmfb"])
+        vmfb_filenames.extend([stem + "_" + target + ".vmfb"])
     return filter_by_model(vmfb_filenames, model)
 
 
