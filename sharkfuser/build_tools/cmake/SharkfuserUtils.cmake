5d889857cd6ab6f6d5b83c54db5805a2ce6f04df
AaronStGeorge <aaronstgeorge@gmail.com>
2025-07-24 00:37:10 +0000

    Address comments

--- sharkfuser/build_tools/cmake/SharkfuserUtils.cmake
+++ sharkfuser/build_tools/cmake/SharkfuserUtils.cmake
@@ -1,3 +1,9 @@
+# Copyright 2025 Advanced Micro Devices, Inc.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 # Usage: sharkfuser_find_external_tool(TOOL_NAME [EXTRA_ERROR_MESSAGE])
 macro(sharkfuser_find_external_tool TOOL_NAME)
   # Parse optional extra error message
@@ -15,7 +21,7 @@ macro(sharkfuser_find_external_tool TOOL_NAME)
   if(NOT ${_FULL_VAR_NAME})
     find_program(${_FULL_VAR_NAME} NAMES ${TOOL_NAME})
     if(NOT ${_FULL_VAR_NAME})
-      message(FATAL_ERROR "Could not find '${TOOL_NAME}' in PATH.${_EXTRA_ERROR_MSG}")
+      message(FATAL_ERROR "Could not find '${TOOL_NAME}' in PATH. ${_EXTRA_ERROR_MSG}")
     endif()
   endif()
   message(STATUS "Using ${TOOL_NAME}: ${${_FULL_VAR_NAME}}")
