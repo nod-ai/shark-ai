c6f2324f494acaed6ae4773bfd3ca00e0df67d2c
Ean Garvey <ean.garvey@amd.com>
2025-07-30 20:12:10 +0000

    Fixup spin lock.

--- shortfin/python/shortfin_apps/sd/components/service.py
+++ shortfin/python/shortfin_apps/sd/components/service.py
@@ -650,7 +650,7 @@ def check_host_array(host_array):
         if host_array.dtype == sfnp.float16:
             dtype = np.float16
         arr = np.frombuffer(array, dtype=dtype)
-        if not np.all(arr == 0):
+        if not np.all(arr == 0) and not np.all(arr[:10] == 0) and not np.all(arr[-10:] == 0):
             check_1 = arr
             check_2 = np.frombuffer(array, dtype=dtype)
             if np.array_equal(check_1, check_2):
