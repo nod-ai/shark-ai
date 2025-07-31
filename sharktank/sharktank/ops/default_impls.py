a7b95295d58143a56b74b66f4268c056a2d0ed8e
Kyle Herndon <kyle.herndon@amd.com>
2025-07-31 18:25:21 +0000

    Some fixes, hopefully

--- sharktank/sharktank/ops/default_impls.py
+++ sharktank/sharktank/ops/default_impls.py
@@ -24,6 +24,7 @@ from sharktank.types import (
     BlockScaledI4Layout,
     BlockScaledLayout,
     TensorScaledLayout,
+    QuantizedLayout,
     unbox_tensor,
     AnyTensor,
 )
@@ -566,6 +567,27 @@ def matmul_default(lhs, rhs, *, transpose_rhs: bool) -> Tensor:
         return torch.unflatten(mm, 0, bdims)
 
     return torch.matmul(lhs, rhs)
+
+
+# Scaled dot product attention
+@scaled_dot_product_attention.override(Tensor, Tensor, Tensor, None)
+def scaled_dot_product_attention_torch(
+    q, k, v, a, is_causal, scale, softcap, impl
+) -> Tensor:
+    if impl is not None and impl != "torch":
+        return NotImplemented
+    if softcap is not None:
+        return NotImplemented
+    q = unbox_tensor(q)
+    k = unbox_tensor(k)
+    v = unbox_tensor(v)
+    if a is not None:
+        a = unbox_tensor(a)
+
+    # TODO: plumb dropout through ops
+    return torch.nn.functional.scaled_dot_product_attention(
+        q, k, v, attn_mask=a, dropout_p=0.0, is_causal=is_causal, scale=scale
+    )
 
 
 @mean.override(Tensor)
