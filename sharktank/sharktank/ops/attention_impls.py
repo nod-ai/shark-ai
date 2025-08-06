02d4fc9c65d12b9f49c1cd8e60c53f53998fa9a0
Kyle Herndon <kyle.herndon@amd.com>
2025-08-06 07:04:08 +0000

    Torch?

--- sharktank/sharktank/ops/attention_impls.py
+++ sharktank/sharktank/ops/attention_impls.py
@@ -69,9 +69,9 @@ def scaled_dot_product_attention_decomposed(
             "scaled_dot_product_attention_decomposed: no masking specified (neither explicit mask nor causal), falling back to is_causal"
         )
 
-    attn_weights = ops.softmax(ops.to(attn_weights, dtype=torch.float32), dim=-1)
-    attn_weights = ops.to(attn_weights, dtype=q.dtype)
-    return ops.matmul(attn_weights, v)  # (bs, heads, slen, head_dim)
+    attn_weights = torch.softmax(attn_weights.to(dtype=torch.float32), dim=-1)
+    attn_weights = attn_weights.to(dtype=q.dtype)
+    return torch.matmul(attn_weights, v)  # (bs, heads, slen, head_dim)
 
 
 @scaled_dot_product_attention.override(AnyTensor, AnyTensor, AnyTensor, None)
