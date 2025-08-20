c3195a2cde8dbbf539e20265d23e8d1a668a37d8
Praveen G <praveen.g2@amd.com>
2025-08-20 03:29:27 -0500

    Include azcopy

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -10,6 +10,15 @@ if [[ $1 = "--nightly" ]]; then
           -e "git+https://github.com/iree-org/iree-turbine.git#egg=iree-turbine"
     pip install mistral_common
 
+elif [[ $1 = "--nightly-cpu" ]]; then
+    pip install -r pytorch-cpu-requirements.txt
+    pip install -r requirements.txt -r requirements-iree-pinned.txt -e sharktank/ -e shortfin/
+    pip install -f https://iree.dev/pip-release-links.html --upgrade --pre iree-base-compiler iree-base-runtime iree-turbine
+    pip install -f https://iree.dev/pip-release-links.html --upgrade --pre \
+  		iree-base-compiler iree-base-runtime --src deps \
+  		-e "git+https://github.com/iree-org/iree-turbine.git#egg=iree-turbine"
+    pip install wave-lang==1.0.2
+
 elif [[ $1 = "--stable" ]]; then
     pip install shark-ai[apps]
     pip install scikit-image
@@ -40,5 +49,7 @@ elif [[ $1 = "--source" ]]; then
     cd -
 else
     echo "setenv.sh --nightly : To install nightly release"
+    echo "setenv.sh --nightly-cpu : To install nightly with pytorch cpu release"
     echo "setenv.sh --stable  : To install stable release"
 fi
+
