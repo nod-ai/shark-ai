34b8b47dd87d22626cfcb649c2d77f73f535de18
Kyle Herndon <kyle.herndon@amd.com>
2025-08-21 19:58:16 +0000

    Different defaults

--- sharktank/sharktank/utils/cli.py
+++ sharktank/sharktank/utils/cli.py
@@ -72,13 +72,13 @@ def add_model_options(parser: argparse.ArgumentParser):
     parser.add_argument(
         "--attention-kernel",
         type=str,
-        default=None,
+        default="decomposed",
         choices=["decomposed", "torch", "sharktank"],
     )
     parser.add_argument(
         "--matmul-kernel",
         type=str,
-        default=None,
+        default="torch",
         choices=["torch", "sharktank", "sharktank.wave", "sharktank.asm"],
     )
     parser.add_argument(
