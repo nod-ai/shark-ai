c7981c4c5a1f3406747233becc8f331bc820ee76
jinchen62 <jinchenye62@gmail.com>
2025-08-22 17:24:56 -0500

    Fix scales and bias padding

--- sharktank/sharktank/kernels/gemm_fp4_asm.py
+++ sharktank/sharktank/kernels/gemm_fp4_asm.py
@@ -62,9 +62,18 @@ module {{
         %k_e8m0 = tensor.dim %arg2, %c1 : tensor<?x?xi8>
         %k = arith.muli %k_f4x2, %c2 : index
         // m_256 = (m + 255) // 256 * 256
-        %add = arith.addi %m, %c255 : index
-        %div = arith.divui %add, %c256 : index
-        %m_256 = arith.muli %div, %c256 : index
+        %m_256 = affine.apply affine_map<()[s0] -> (s0 ceildiv 256 * 256)>()[%m]
+        %hi_pad = arith.subi %m_256, %m : index
+        %c0_i8 = arith.constant 1 : i8
+        %x_scales_padded = tensor.pad %arg2 low[%c0, %c0] high[%hi_pad, %c0] {{
+        ^bb0(%i0: index, %i1: index):
+            tensor.yield %c0_i8 : i8
+        }} : tensor<?x?xi8> to tensor<?x?xi8>
+        %c0_f32 = arith.constant 1.0 : f32
+        %bias_padded = tensor.pad %arg4 low[%c0, %c0] high[%hi_pad, %c0] {{
+        ^bb0(%i0: index, %i1: index):
+            tensor.yield %c0_f32 : f32
+        }} : tensor<?x?xf32> to tensor<?x?xf32>
         %alpha = arith.constant 1.0 : f32
         %beta = arith.constant 0.0 : f32
         %alpha_i32 = arith.bitcast %alpha : f32 to i32
@@ -73,7 +82,7 @@ module {{
         %n_i32 = arith.index_cast %n : index to i32
         %k_i32 = arith.index_cast %k : index to i32
         %k_e8m0_i32 = arith.index_cast %k_e8m0 : index to i32
-        %gemm = hal.dispatch.extern "f4gemm_kernel_func"[%m, %n](%alpha_i32, %beta_i32, %k_i32, %k_i32, %n_i32, %m_i32, %n_i32, %k_i32, %k_e8m0_i32, %k_e8m0_i32, %arg0, %arg1, %arg2, %arg3, %arg4) : (i32, i32, i32, i32, i32, i32, i32, i32, i32, i32, tensor<?x?xi8>{{%m, %k_f4x2}}, tensor<?x?xi8>{{%n, %k_f4x2}}, tensor<?x?xi8>{{%m, %k_e8m0}}, tensor<?x?xi8>{{%n, %k_e8m0}}, tensor<?x?xf32>{{%m, %n}}) -> tensor<?x?xbf16>{{%m_256, %n}}
+        %gemm = hal.dispatch.extern "f4gemm_kernel_func"[%m, %n](%alpha_i32, %beta_i32, %k_i32, %k_i32, %n_i32, %m_i32, %n_i32, %k_i32, %k_e8m0_i32, %k_e8m0_i32, %arg0, %arg1, %x_scales_padded, %arg3, %bias_padded) : (i32, i32, i32, i32, i32, i32, i32, i32, i32, i32, tensor<?x?xi8>{{%m, %k_f4x2}}, tensor<?x?xi8>{{%n, %k_f4x2}}, tensor<?x?xi8>{{%m_256, %k_e8m0}}, tensor<?x?xi8>{{%n, %k_e8m0}}, tensor<?x?xf32>{{%m_256, %n}}) -> tensor<?x?xbf16>{{%m_256, %n}}
             count(%device: !hal.device, %m_workload: index, %n_workload: index) -> (index, index, index) {{
                 %c1_0 = arith.constant 1 : index
                 %subm = arith.constant 256 : index
@@ -166,7 +175,7 @@ module {{
 {{% endraw %}}
     util.func private @{{{{kernel_name}}}}(%x: !x, %w: !w, %x_scale: !x_scale, %w_scale: !w_scale, %bias: !bias) -> !result {{
         %x_scale_shuffle = util.call @shuffle_scales(%x_scale) : (!x_scale) -> !x_scale
-        %w_scale_shuffle = util.call @shuffle_scales(%w_scale) : (!w_scale) -> !x_scale
+        %w_scale_shuffle = util.call @shuffle_scales(%w_scale) : (!w_scale) -> !w_scale
         %result = util.call @asm_mxfp4_gemm(%x, %w, %x_scale_shuffle, %w_scale_shuffle, %bias) : (!x, !w, !x_scale, !x_scale, !bias) -> !result
         util.return %result : !result
     }}
