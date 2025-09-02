5f48a4c591ab313dee811db17f02243868627651
dezhliao <dezhi.liao@amd.com>
2025-09-02 15:40:43 -0700

    Investigate CI failure3
    Signed-off-by: dezhliao <dezhi.liao@amd.com>

--- sharktank/tests/layers/paged_llama_attention_block_test.py
+++ sharktank/tests/layers/paged_llama_attention_block_test.py
@@ -9,7 +9,7 @@ from sharktank.layers.paged_attention import CacheAllocation
 import unittest
 import torch
 from iree.turbine import aot
-from sharktank.layers.paged_llama_attention_block import *
+from sharktank.layers.paged_llama_attention_block import PagedLlamaAttentionBlock, create_paged_llama_attention_block
 from sharktank.layers import (
     PagedAttention,
     build_rotary_layer,
