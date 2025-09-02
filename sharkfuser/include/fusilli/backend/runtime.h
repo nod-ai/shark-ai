e72e688c8ef05e12a1960077aac9da9ea9fc16aa
Sambhav Jain <sambhav.jain@amd.com>
2025-09-02 22:44:24 +0000

    switch to using static weak_ptr

--- sharkfuser/include/fusilli/backend/runtime.h
+++ sharkfuser/include/fusilli/backend/runtime.h
@@ -27,31 +27,43 @@
 namespace fusilli {
 
 // Create static singleton IREE runtime instance shared across handles/threads
-// TODO(sjain-stanford): Consider moving to `std::call_once` to avoid
-// paying the cost of acquiring/releasing the mutex lock on every call
-// to `FusilliHandle::createSharedInstance()`. The only minor issue is the
-// lambda for `call_once` expects a void callable but in our case we
-// return ErrorObject inside `FUSILLI_CHECK_ERROR` so it might need
-// some restructuring to properly capture/propagate the error state.
 inline ErrorOr<IreeRuntimeInstanceSharedPtrType>
 FusilliHandle::createSharedInstance() {
-  // Mutex for thread-safe initialization of sharedInstance
+  // Mutex for thread-safe initialization of weakInstance
   static std::mutex instanceMutex;
-  static IreeRuntimeInstanceSharedPtrType sharedInstance;
 
+  // Static weak_ptr to the IREE runtime instance ensures that the
+  // instance is only created once and shared across all handles
+  // without prolonging its lifetime till program termination. This
+  // allows the instance to be released when the last handle owning
+  // it goes out of scope, as opposed to hogging on to it until the
+  // static variable goes out of scope upon program termination.
+  static IreeRuntimeInstanceWeakPtrType weakInstance;
+
+  // If multiple threads simultaneously request a handle, they will
+  // race into `createSharedInstance()` but only one will succeed in
+  // creating the instance, and others will use it.
   std::lock_guard<std::mutex> lock(instanceMutex);
+
+  // Try to lock the weak_ptr to get the shared_ptr.
+  IreeRuntimeInstanceSharedPtrType sharedInstance = weakInstance.lock();
+
+  // If weak_ptr expired, it means no handles are alive (and holding the
+  // instance), create a new one.
   if (sharedInstance == nullptr) {
     FUSILLI_LOG_LABEL_ENDL("INFO: Creating shared IREE runtime instance");
     iree_runtime_instance_options_t opts;
     iree_runtime_instance_options_initialize(&opts);
     iree_runtime_instance_options_use_all_available_drivers(&opts);
-    iree_runtime_instance_t *rawInstance = nullptr;
 
+    iree_runtime_instance_t *rawInstance = nullptr;
     FUSILLI_CHECK_ERROR(iree_runtime_instance_create(
         &opts, iree_allocator_system(), &rawInstance));
 
     sharedInstance = IreeRuntimeInstanceSharedPtrType(
         rawInstance, IreeRuntimeInstanceDeleter());
+
+    weakInstance = sharedInstance;
   }
 
   return ok(sharedInstance);
@@ -60,8 +72,8 @@ FusilliHandle::createSharedInstance() {
 // Create IREE HAL device for this handle
 inline ErrorObject FusilliHandle::createPerHandleDevice() {
   FUSILLI_LOG_LABEL_ENDL("INFO: Creating per-handle IREE HAL device");
-  iree_hal_device_t *rawDevice = nullptr;
 
+  iree_hal_device_t *rawDevice = nullptr;
   FUSILLI_CHECK_ERROR(iree_runtime_instance_try_create_default_device(
       instance_.get(), iree_make_cstring_view(halDriver.at(backend_)),
       &rawDevice));
@@ -71,16 +83,16 @@ inline ErrorObject FusilliHandle::createPerHandleDevice() {
 }
 
 // Create IREE runtime session for this graph (if not available already)
-// and load the compiled artifact into it
+// and load the compiled artifact into it.
 inline ErrorObject Graph::createPerGraphSession(const FusilliHandle &handle,
                                                 const std::string &vmfbPath) {
-  // Skip to loading if session is already created
+  // Skip to loading if session is already created.
   if (session_ == nullptr) {
     FUSILLI_LOG_LABEL_ENDL("INFO: Creating per-graph IREE runtime session");
     iree_runtime_session_options_t opts;
     iree_runtime_session_options_initialize(&opts);
-    iree_runtime_session_t *rawSession = nullptr;
 
+    iree_runtime_session_t *rawSession = nullptr;
     FUSILLI_CHECK_ERROR(iree_runtime_session_create_with_device(
         handle.getInstance(), &opts, handle.getDevice(),
         iree_runtime_instance_host_allocator(handle.getInstance()),
