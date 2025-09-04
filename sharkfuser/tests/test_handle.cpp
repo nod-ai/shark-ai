d0dd53b9f1149ead7c5b41e40a2564bf45c90015
Sambhav Jain <sambhav.jain@amd.com>
2025-09-04 19:05:39 +0000

    handle test

---
+++ sharkfuser/tests/test_handle.cpp
@@ -0,0 +1,71 @@
+// Copyright 2025 Advanced Micro Devices, Inc.
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <fusilli.h>
+
+#include "utils.h"
+
+#include <atomic>
+#include <catch2/catch_test_macros.hpp>
+#include <cstddef>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+using namespace fusilli;
+
+TEST_CASE("Single FusilliHandle creation", "[handle]") {
+  SECTION("CPU handle") {
+    FusilliHandle handle =
+        FUSILLI_REQUIRE_UNWRAP(FusilliHandle::create(Backend::CPU));
+  }
+#ifdef FUSILLI_ENABLE_AMDGPU
+  SECTION("GPU handle") {
+    FusilliHandle handle =
+        FUSILLI_REQUIRE_UNWRAP(FusilliHandle::create(Backend::GFX942));
+  }
+#endif
+}
+
+TEST_CASE("Multiple FusilliHandle creation", "[handle]") {
+  FusilliHandle handle1 =
+      FUSILLI_REQUIRE_UNWRAP(FusilliHandle::create(Backend::CPU));
+#ifdef FUSILLI_ENABLE_AMDGPU
+  FusilliHandle handle2 =
+      FUSILLI_REQUIRE_UNWRAP(FusilliHandle::create(Backend::GFX942));
+#endif
+}
+
+TEST_CASE("Multi-threaded FusilliHandle creation", "[handle][thread]") {
+  constexpr int kNumThreads = 16;
+
+  std::vector<FusilliHandle> handles;
+  handles.reserve(kNumThreads);
+
+  std::vector<std::thread> threads;
+  threads.reserve(kNumThreads);
+
+  std::mutex handlesMutex;
+  std::atomic<bool> creationFailed{false};
+
+  for (size_t i = 0; i < kNumThreads; ++i) {
+    threads.emplace_back([&]() {
+      auto handleOrError = FusilliHandle::create(Backend::CPU);
+      if (isError(handleOrError)) {
+        creationFailed.store(true);
+        return;
+      }
+      std::lock_guard<std::mutex> lock(handlesMutex);
+      handles.push_back(std::move(*handleOrError));
+    });
+  }
+
+  for (auto &t : threads)
+    t.join();
+
+  REQUIRE(!creationFailed.load());
+  REQUIRE(handles.size() == kNumThreads);
+}
