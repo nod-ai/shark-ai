2c94b6c026b06d4642b967d7ea05e0ca15d9246b
Yash Rathore <yrathore@amd.com>
2025-09-05 04:55:01 +0000

    Add More Checks

--- scripts/run_serving.sh
+++ scripts/run_serving.sh
@@ -141,6 +141,13 @@ curl http://localhost:$port/generate \
                 "sampling_params": {"max_completion_tokens": 50}
             }' >> $(pwd)/../output_artifacts/online_serving.log
 
+curl http://localhost:$port/generate \
+           -H "Content-Type: application/json" \
+           -d '{
+              "text": "<|begin_of_text|>Give the first ten numbers of the Fibonacci series.<|eot_id|>",
+                "sampling_params": {"max_completion_tokens": 50}
+            }' >> $(pwd)/../output_artifacts/online_serving.log
+
 sleep 10
 kill -9 $shortfin_process
 
