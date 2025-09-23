989adedca261f40ed8d42ed829313d715765009d
Ean Garvey <ean.garvey@amd.com>
2025-09-23 17:38:06 +0000

    Address comments and add golden generation/upload scripts and documentation.

--- sharktank/sharktank/models/wan/attention.py
+++ sharktank/sharktank/models/wan/attention.py
@@ -1,4 +1,9 @@
-# Copyright 2024-2025 The Alibaba Wan Team Authors. All rights reserved.
+# Copyright 2025 Advanced Micro Devices, Inc.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 import torch
 from sharktank import ops
 
