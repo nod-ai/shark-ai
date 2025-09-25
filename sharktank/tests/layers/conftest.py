0fece34126ea107ee9b1ac71224f7fe4c22c6d1b
PhaneeshB <b.phaneesh@gmail.com>
2025-09-25 13:45:10 +0000

    add copyright txt

--- sharktank/tests/layers/conftest.py
+++ sharktank/tests/layers/conftest.py
@@ -1,3 +1,9 @@
+# Copyright 2024 Advanced Micro Devices, Inc.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 def pytest_addoption(parser):
     """Add command line option for IRPA file path."""
     parser.addoption(
