6609be986cb6826e6734adcdd6aff91bf6a67458
PhaneeshB <b.phaneesh@gmail.com>
2025-09-25 14:50:27 +0000

    move manual seed to test files

--- sharktank/tests/layers/linear_with_iree_test.py
+++ sharktank/tests/layers/linear_with_iree_test.py
@@ -19,7 +19,7 @@ class Linear(torch.nn.Module):
 
 @pytest.mark.parametrize("dtype,atol", [(torch.float32, 1e-4), (torch.float16, 1e-4)])
 def test_linear_iree_vs_eager(dtype, atol):
-    torch.manual_seed(0)
+    torch.manual_seed(42)
     m = Linear(64, 64, bias=False, dtype=dtype)
     x = torch.randn(2, 8, 64, dtype=dtype)
     run_iree_vs_torch_fx(m, args=(x,), atol=atol, rtol=0)
