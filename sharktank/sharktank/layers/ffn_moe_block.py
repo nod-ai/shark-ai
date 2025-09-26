75fe31abe5a501f0d032d1bdb517616815096246
sonbol <oyazdanb@amd.com>
2025-09-26 16:33:24 -0700

    vmfb compiles but need to activate biases

--- sharktank/sharktank/layers/ffn_moe_block.py
+++ sharktank/sharktank/layers/ffn_moe_block.py
@@ -85,7 +85,8 @@ class PreGatherFFNMOE(ThetaLayer):
         )
         output = einsum_2args(oh, matmul, "bm,bmn->mn")
         return output
-
+    
+    
     def forward(
         self,
         h: torch.Tensor,  # (bs * sl, feature_dim)
@@ -106,17 +107,16 @@ class PreGatherFFNMOE(ThetaLayer):
         """
         # bs: batch_size
         # sl: sequence_length
-
-        gate_w = self.ffn_gate[experts]  # (B, K, C, D)
-        up_w = self.ffn_up[experts]  # (B, K, C, D)
+        gate_w =  self.ffn_gate[experts,:,:]  # (N,K,C,D)
+        up_w   =  self.ffn_up[experts,:,:]     # (N,K,C,D)
 
         h_expanded = h.unsqueeze(1).unsqueeze(1)
         gate_proj = (gate_w * h_expanded).sum(-1)
         up_proj = (up_w * h_expanded).sum(-1)
-        if self.ffn_gate_bias is not None:
-            gate_proj = gate_proj + self.ffn_gate_bias[experts]
+        '''if self.ffn_gate_bias is not None:
+            gate_proj = gate_proj + self.ffn_gate_bias[experts,:]  # (N,K,C)
         if self.ffn_up_bias is not None:
-            up_proj = up_proj + self.ffn_up_bias[experts]
+            up_proj = up_proj + self.ffn_up_bias[experts,:]'''
 
         if self.use_moe_swiglu:
             # Concatenate after separate projections for swiglu
@@ -129,12 +129,11 @@ class PreGatherFFNMOE(ThetaLayer):
         if self.model_arch == "llama4":
             hidden = hidden * expert_gate.unsqueeze(-1)
 
-        down_w = self.ffn_down[experts]
+        down_w =  self.ffn_down[experts,:,:]   # (N,K,D_out,C)
         t2 = (down_w * hidden.unsqueeze(2)).sum(-1)
 
-        if self.ffn_down_bias is not None:
-            t2 = t2 + self.ffn_down_bias[experts]
-
+        '''if self.ffn_down_bias is not None:
+            t2 = t2 + self.ffn_down_bias[experts,:]  # (N,K,D_out)'''
         out = (t2 * expert_gate.unsqueeze(-1)).sum(1)
         return out
 
