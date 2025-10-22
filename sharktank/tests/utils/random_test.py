f8b4360e3d276d3df558867d0145879cc95ae1d7
Sonbol Yazdanbakhsh <oyazdanb@amd.com>
2025-10-22 17:53:31 +0000

    adding header to random_test.py

--- sharktank/tests/utils/random_test.py
+++ sharktank/tests/utils/random_test.py
@@ -1,5 +1,10 @@
-import math
+# Copyright 2025 Advanced Micro Devices, Inc.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+import math
 import torch
 
 from sharktank.utils.random import (
