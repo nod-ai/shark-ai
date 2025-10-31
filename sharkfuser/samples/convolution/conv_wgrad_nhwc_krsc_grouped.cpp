2068772d126296005b10dcbdce505a4857ddf128
Sambhav Jain <sambhav.jain@amd.com>
2025-10-31 09:41:04 -0700

    Apply suggestion from @sjain-stanford

--- sharkfuser/samples/convolution/conv_wgrad_nhwc_krsc_grouped.cpp
+++ sharkfuser/samples/convolution/conv_wgrad_nhwc_krsc_grouped.cpp
@@ -20,7 +20,7 @@
 using namespace fusilli;
 
 // TODO(#2579): This test is mark as "shouldfail" due to incorrect lowering of
-//              not unite-stide Grouped ConvWGrad in IREE.
+//              not unit-stide Grouped ConvWGrad in IREE.
 //              Please remove this tag when IREE supports this case.
 TEST_CASE("Convolution wgrad; DY/X (NHWC), DW (KRSC); 1x1; no padding; grouped",
           "[conv][graph][!shouldfail]") {
