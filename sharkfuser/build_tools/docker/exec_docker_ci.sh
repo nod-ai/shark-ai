e29015ce23c8755783121cb5a8d5e01e5f4b805f
Sambhav Jain <sambhav.jain@amd.com>
2025-10-31 18:50:32 +0000

    bump docker

--- sharkfuser/build_tools/docker/exec_docker_ci.sh
+++ sharkfuser/build_tools/docker/exec_docker_ci.sh
@@ -20,5 +20,5 @@ docker run --rm \
            -v "${PWD}":/workspace \
            ${DOCKER_RUN_DEVICE_OPTS} \
            --security-opt seccomp=unconfined \
-           ghcr.io/sjain-stanford/compiler-dev-ubuntu-24.04:main@sha256:b485cc3580d60ac03640c6190a3d3c8133dafe50d7ab06160a7df90ab32bb45d \
+           ghcr.io/sjain-stanford/compiler-dev-ubuntu-24.04:main@sha256:430a1afb2d14ec2cbe542745a947f27638181ab2336afdaca0dbc803358ebe71 \
            "$@"
