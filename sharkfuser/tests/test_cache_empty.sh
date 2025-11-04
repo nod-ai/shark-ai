fd39601c548e8d77f2fba0dfb3bfbe5adfe2ea86
Sambhav Jain <sambhav.jain@amd.com>
2025-11-04 18:43:13 +0000

    only check /tmp for cache files

--- sharkfuser/tests/test_cache_empty.sh
+++ sharkfuser/tests/test_cache_empty.sh
@@ -6,8 +6,7 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-# Use ${FUSILLI_CACHE_DIR} if set, otherwise default to /tmp
-CACHE_DIR="${FUSILLI_CACHE_DIR:-/tmp}/.cache/fusilli"
+CACHE_DIR="/tmp/.cache/fusilli"
 
 if [ ! -d "${CACHE_DIR}" ]; then
 	echo "cache directory ${CACHE_DIR} should exist after running tests"
